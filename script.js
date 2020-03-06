e28fc1ec4ed68124bb22145b3b3d2a235f4cabab
Katya Sheps <katya@sheps.ru>
2020-03-06 22:58:08 +0300

    На info навешан обработчик событий addEventListener с callback функцией с делегированием и поиском по target

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.addEventListener('DOMContentLoaded', function () {
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
+
     function hideTabContent(a) {
-        for (let i = a; i < tabContent.length, i++) {
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
@@ -14,10 +15,23 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
-            tabContent[i].classList.remove('hide');
-            tabContent[i].classList.add('show');
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
         }
 
     }
 
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    })
 });
